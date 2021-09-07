176793784daa9b9976bfcc24388f80f4f072ee83
Emiliano <emiliano.quiroga093@gmail.com>
2021-09-06 21:55:55 -0300

    stadistics component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from './components/Header'
 import Hero from "./components/Hero";
+import Stadistics from "./components/Stadistics";
 
 export default function App () {
     return (
         <React.Fragment>
             <Header />
             <Hero />
+            <Stadistics />
         </React.Fragment>
     )
 }
