36cedd04d9606ed0915b8338475addba7d460eb3
Emiliano <emiliano.quiroga093@gmail.com>
2021-09-07 12:49:30 -0300

    about section done, product component and styles done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from './components/Header'
 import Hero from "./components/Hero";
 import Stadistics from "./components/Stadistics";
+import About from "./components/About";
 
 export default function App () {
     return (
@@ -9,6 +10,7 @@ export default function App () {
             <Header />
             <Hero />
             <Stadistics />
+            <About />
         </React.Fragment>
     )
 }
