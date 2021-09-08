5ebad9e70e7687403b92de02f2f1b5eb17d5e84b
Emiliano <emiliano.quiroga093@gmail.com>
2021-09-08 15:15:57 -0300

    fixed out of stock option input to disabled

--- src/components/Product-Input.js
+++ src/components/Product-Input.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-export default function Input({id, title, pledge, description, numberLeft}){
+export default function Input({id, title, pledge, description, numberLeft, stock}){
     return(
-     <div className="input-element">
+     <div className={stock === true? "input-element" : "input-element no-stock"}>
         <div className="input-wrapper">
         <input 
         type="checkbox"
         id={id}
+        disabled={stock === false? "disabled" : ""}
         ></input>    
         <label htmlFor={id} className="title">
            {title} 
@@ -14,10 +15,15 @@ export default function Input({id, title, pledge, description, numberLeft}){
         <p className="pledge">{ pledge }</p>
         </div>
         <p className="pledge-description"> {description} </p>
+        {numberLeft
+        ?
         <div className="left-wrapper">
-            <h4> {numberLeft} </h4>
-            <p className="left">left</p>
-         </div>
+        <h4> {numberLeft} </h4>
+        <p className="left">left</p>
+        </div>:
+        <React.Fragment></React.Fragment>
+        }
+        
      </div>
     )
 }
--- src/components/Project-Modal.js
+++ src/components/Project-Modal.js
@@ -8,6 +8,7 @@ return (
         <h2>Back this project</h2>
         <p className="project-description">Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
         <Input
+        stock={true}
         id="no-reward" 
         title="Pledge with no reward"
         pledge=""
@@ -15,6 +16,7 @@ return (
         description="Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email."
         />
         <Input
+        stock={true}
         id="bamboo-stand" 
         title="Bamboo Stand"
         pledge="Pledge $25 or more"
@@ -22,6 +24,7 @@ return (
         description="You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list."
         />
         <Input
+        stock={true}
         id="black-edition" 
         title="Black Edition Stand"
         pledge="Pledge $75 or more"
@@ -29,6 +32,7 @@ return (
         description="You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included."
         />
         <Input
+        stock={false}
         id="mahogany" 
         title="Mahogany Special Edition"
         pledge="Pledge $200 or more"
