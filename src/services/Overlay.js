6ac12627970ee9383764ffda742d3e8e0b82536b
Emiliano <emiliano.quiroga093@gmail.com>
2021-09-21 11:54:01 -0300

    modal working

--- src/services/Overlay.js
+++ src/services/Overlay.js
@@ -1,6 +1,5 @@
 export const body = document.getElementById("body")
 export const overlayDiv = document.getElementById("overlay")
-const portal = document.getElementById('portal')
 
 
 export function openMenu() {
@@ -13,12 +12,4 @@ export function closeMenu() {
     body.classList.remove('no-scroll');
     overlayDiv.classList.remove("fade-in")
     overlayDiv.classList.add("fade-out")
-}
-
-export function modalOpen (){
-    if (portal.lastChild.lastChild.classList.contains('modal-container')){
-        body.classList.add('no-scroll');
-        overlayDiv.classList.remove("fade-out")
-        overlayDiv.classList.add("fade-in")
-    }
 }
